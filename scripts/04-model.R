f6557c6a4ed723ffdea459158610a1b141103761
thu <thu.dong@mail.utoronto.ca>
2024-04-21 02:12:57 +0000

    Update model script

--- scripts/04-model.R
+++ scripts/04-model.R
@@ -1,11 +1,10 @@
 #### Preamble ####
-# Purpose: Models... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Models for young adults and middle-aged adults
+# Author: Thu Dong
+# Date: 20 April, 2024
+# Contact: thu.dong@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+
 
 
 #### Workspace setup ####
